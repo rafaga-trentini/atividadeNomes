3b9c7cffe795f97ce151b28e13e8b855fcc6fe67
Ráfaga dos Santos Trentini <rafaga.trentini@gmail.com>
2022-08-17 10:18:35 -0300

    Add files via upload

---
+++ src/atividadeNomes/MainThread.java
@@ -0,0 +1,26 @@
+package atividadeNomes;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
+public class MainThread {
+
+	public static void main(String[] args) throws FileNotFoundException, InterruptedException {
+		Scanner lerNome = new Scanner(System.in);
+		System.out.print("Informe um nome: ");
+		String nome = lerNome.next();
+		nome.toLowerCase();
+		long inicio = System.currentTimeMillis();
+		Thread minhaThread = new Thread();
+		for (int i = 0; i <10; i++) {
+			minhaThread = new Thread(new atividadeNomeThread(nome, i));
+			minhaThread.start();
+			
+		}
+		minhaThread.join();
+		long fim = System.currentTimeMillis();
+		System.out.println("Tempo decorrido: "+((fim-inicio)));
+	}
+
+}
